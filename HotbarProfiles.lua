739bd61f32b55784e03369eda4df84f7026c2ce0
gubriansky@sernato.sk <gubriansky@sernato.sk>
2020-09-23 19:51:33 +0200

    adding frame to save and load profiles also a bit of debuging

--- HotbarProfiles.lua
+++ HotbarProfiles.lua
@@ -1,15 +1,6 @@
 local AceGUI = LibStub("AceGUI-3.0")
 
---[[local frame = AceGUI:Create("Frame")
-frame:SetTitle("Mike debug frame")
-frame:SetStatusText("Debug ready")
-
-local function log(msg)
-    local desc = AceGUI:Create("Label")
-    desc:SetText(msg)
-    desc:SetFullWidth(true)
-    frame:AddChild(desc)
-end--]]
+ABP_MAX_ACTION_BUTTONS = 120
 
 HotbarProfiles = LibStub("AceAddon-3.0"):NewAddon("HotbarProfiles", "AceConsole-3.0")
 
@@ -17,14 +8,66 @@ local function log(msg)
     HotbarProfiles:Print(msg)
 end
 
+HotbarPopup = AceGUI:Create("Frame")
+
+local function toggleHBP()
+    HotbarProfiles.db.profile.drop.hide = not HotbarProfiles.db.profile.drop.hide
+    log(HotbarProfiles.db.profile.drop.hide)
+    if HotbarProfiles.db.profile.drop.hide then
+        HotbarPopup:Hide()
+    else
+        HotbarPopup:Show()
+    end
+end
+
+function HotbarPopup:createHotbarPopup()
+    self:SetTitle("HotbarProfiles")
+
+    self:SetCallback("OnClose", function(widget)
+        HotbarProfiles.db.profile.drop.hide = true
+        self:Hide()
+    end)
+
+    local saveActionBars = AceGUI:Create("Label")
+    saveActionBars:SetText("Enter name to save the current action bars")
+    self:AddChild(saveActionBars)
+
+    local saveABName = AceGUI:Create("EditBox")
+    saveABName:SetLabel("Name:")
+    saveABName:SetCallback("OnEnterPressed", function(widget, event, text)
+        log(text)
+    end)
+    self:AddChild(saveABName)
+
+    local loadButton = AceGUI:Create("Button")
+    loadButton:SetText("Load")
+    loadButton:SetCallback("OnClick", function()
+        local slot
+        for slot = 1, ABP_MAX_ACTION_BUTTONS do
+            local type, id, sub = GetActionInfo(slot)
+            log(type)
+            log(id)
+            log(sub)
+        end
+    end)
+    self:AddChild(loadButton)
+end
+
+HotbarPopup:createHotbarPopup()
+
 log("HotbarProfiles use /hbp to enable the addon")
 
 local hpLDB = LibStub("LibDataBroker-1.1"):NewDataObject("HotbarProfiles", {
     type = "data source",
     text = "HotbarProfiles",
     icon = "Interface\\Icons\\INV_Chest_Cloth_17",
-    OnClick = function() print("HotbarProfiles addon by Mike") end,
+    OnClick = function()
+        log("HotbarProfiles addon by Mike")
+        toggleHBP()
+    end,
 })
+
+HotbarPopup:Hide()
 
 log("Created data object")
 
@@ -40,6 +83,9 @@ function HotbarProfiles:OnInitialize()
             minimap = {
                 hide = false,
             },
+            drop = {
+                hide = true,
+            },
         },
     })
     icon:Register("HotbarProfiles", hpLDB, self.db.profile.minimap)
@@ -47,14 +93,9 @@ function HotbarProfiles:OnInitialize()
     log("Calling addon init done")
 end
 
+icon:Show("HotbarProfiles")
+
 function HotbarProfiles:HotbarProfilesControll()
     log("Calling addon /hbp callback")
-    self.db.profile.minimap.hide = not self.db.profile.minimap.hide
-    if self.db.profile.minimap.hide then
-        icon:Hide("HotbarProfiles")
-    else
-        icon:Show("HotbarProfiles")
-    end
-
-    log("Calling addon /hbp callback done")
+    toggleHBP()
 end
